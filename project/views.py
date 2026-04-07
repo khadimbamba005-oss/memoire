d88152b69ea1bd5d9e03bde7f2a09ecd488d49b9
khadimkane <khadimbamba005@gmail.com>
2026-04-07 07:53:00 +0000

    Mise a jour

--- project/views.py
+++ project/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
 
 
 def connexion(request):
+    return render(request , 'login.html')
+
+
+def connexion(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        print("username:" , username)
+        print("Password:", password)
+
+
+        user = authenticate(request,username=username ,password = password)
+
+        if user is not None:
+            print("Connexion reussi")
+            login(request,user)
+            return redirect('dashoboard_admin')
+        else:
+            print("Echec de la connexion")
+            return render(request , 'login.html',{
+                'error':'Indentifiants incorrects'
+            })
     return render(request , 'login.html')
